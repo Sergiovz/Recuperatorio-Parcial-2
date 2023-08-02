e88358f72a4ae9eec5c3f909c4c3b5fca534d592
sergio valdez <sergiovaldez599@gmail.com>
2023-08-02 15:12:26 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// Imports
+const cors = require('cors');
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const path = require('path');
+require('dotenv').config();//configuración para variables de entorno
+require('ejs');//acceso al motor de vistas de ejs
+
+const app = express();
+const port = process.env.PORT || 3000
+
+// Middlewares
+// TODO: Implementar middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use(require('./routes/reserva.routes'));
+
+// TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use((req, res, next) => {
+    return res.status(404).render('404.ejs');
+  });
+
+// Starting the server
+app.listen(port, () => console.log(`Servidor corriendo en el puerto http://localhost:${port}`));
