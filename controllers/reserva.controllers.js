e783f2d803ee5fda6882b95652c11db069d78109
sergio valdez <sergiovaldez599@gmail.com>
2023-08-02 16:24:23 -0300

    controladores para CRUD de reservas completado

--- controllers/reserva.controllers.js
+++ controllers/reserva.controllers.js
@@ -1,4 +1,5 @@
 // TODO: Crear controladores para cada una de las rutas de reserva.
+const Reserva = require('../models/Reserva')
 const ctrl = {};
 
 // ==========================================
@@ -6,31 +7,150 @@ const ctrl = {};
 // ==========================================
 
 // Obtener todas las reservas
-ctrl.
+ctrl.renderLista = (req, res) => {
+    res.render("lista")
+}
 // Formulario para crear una reserva
-ctrl.
-
+ctrl.renderNuevo = (req, res) => {
+    res.render("nuevo")
+}
 // Formulario para editar una reserva
-ctrl.
+ctrl.renderEditar = (req, res) => {
+    const { id } = req.params
+    res.render("editar", { id })
+}
 
 // ==========================================
 //     Controladores para CRUD de reservas
 // ==========================================
 
 // Obtener todas las reservas
-ctrl.
+ctrl.obtenerReservas = async(req, res) => {
+    
+    try {
+        const reservas = await Reserva.findAll({
+            where: {
+                estado: true
+            }
+        });
+    
+        return res.json(reservas);
+        
+    } catch (error) {
+        console.log("Error al obtener los datos de las reservas", error);
+        return res.status(500).json({
+            message: 'Erro al obtener los datos de las reservas'
+        });
+        
+    }
+};
 
 // Obtener una reserva
-ctrl.
+ctrl.obtenerReserva = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const reserva = await Reserva.findByPk(id);
+      return res.json(reserva);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "Error al obtener la reserva",
+      });
+    }
+  };
 
 // Crear una reserva
-ctrl.
+ctrl.crearReserva = async (req, res) => {
+    const {
+      nombre,
+      apellido,
+      nombrePelicula,
+      fechaIngreso,
+      fechaSalida,
+      sala,
+      asiento,
+      precio
+    } = req.body;
+    try{
+      const nuevaReserva = new Reserva({
+        nombre,
+        apellido,
+        nombrePelicula,
+        fechaIngreso,
+        fechaSalida,
+        sala,
+        asiento,
+        precio
+      });
+  
+      await nuevaReserva.save();
+  
+      return res.status(201).json({
+        message: 'La reserva fue creada con éxito'
+      });
+  
+    } catch (error) {
+      console.log("Error al crear la reserva", error);
+      return res.status(500).json({
+        message: 'Error al crear la reserva'
+      });
+    }
+  };
 
 // Actualizar una reserva
-ctrl.
+ctrl.actualizarReserva = async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      const reserva = await Reserva.findByPk(id);
+  
+      await reserva.update(req.body);
+  
+      return res.status(201).json({ 
+      message: "Reserva actualizada con éxito"
+    });
+
+    } catch (error) {
+      console.log("Error al actualizar la reserva", error);
+      return res.status(500).json({
+        message: "Error al actualizar la reserva",
+      });
+    }
+  };
 
 // Eliminar una reserva de forma lógica
-ctrl.
+ctrl.eliminarReserva = async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      if (!id) {
+        throw {
+          status: 400,
+          message: "No se ha enviado el id de la reserva",
+        };
+      }
+  
+      const reserva = await Reserva.findByPk(id);
+  
+      if (!reserva) {
+        throw {
+          status: 404,
+          message: "No se encontró la reserva",
+        };
+      }
+  
+      await reserva.update({ estado: false });
+  
+      return res.json({
+        message: "Reserva eliminada correctamente"
+      });
 
+    } catch (error) {
+      console.log("Error al eliminar la reserva", error);
+      return res.status(error.status || 500).json({
+        message: error.message || "Error al eliminar la reserva",
+      });
+    }
+  };
 
 module.exports = ctrl;
